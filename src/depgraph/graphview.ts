08b3d7a8c375beb2c99046675c4777d5ab22b514
Dirk Müller <dirk.mueller92@yahoo.de>
2018-08-02 20:57:07 +0200

    removed graph view

--- src/depgraph/graphview.ts
+++
@@ -1,7 +0,0 @@
-import { TextDocumentContentProvider, CancellationToken, ProviderResult, Uri } from 'vscode'
-
-export class GraphView implements TextDocumentContentProvider {
-    provideTextDocumentContent(uri: Uri, token: CancellationToken): ProviderResult<string> {
-        return 'not yet implemented ' + uri.fsPath
-    }
-}
