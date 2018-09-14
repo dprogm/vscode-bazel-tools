93feee37c4ad4e898266e870c99d57c698bf54e0
Quentin Godeau <quentingodeau@users.noreply.github.com>
2018-09-14 23:29:49 +0200

    Add some bazel module documentation

--- src/bazel.ts
+++ src/bazel.ts
@@ -10,14 +10,22 @@ import { utils } from './utils';
 
 export module bazel {
     export interface BazelQueryItem {
+        /** Kind of the target (example: cc_library, ...) */
         readonly kind: string;
+        /** Bazel valid target (example: //package:target, ...) */
         readonly label: string;
     }
 
+    /** Path to the bazel executable. */
     let bazelExecutablePath: string;
+    /** Packages that should not be considered during querying. */
     let excludedPackages: string[];
 
-    export function init() {
+    /**
+     * This function must be call in the extension initialization.
+     * It will load the configuration and register listener on modification.
+     */
+    export function init(): void {
         Workspace.onDidChangeConfiguration(configurationChangeEvent => {
             if (configurationChangeEvent.affectsConfiguration('bazel')) {
                 loadConfiguration(Workspace.getConfiguration('bazel'));
@@ -27,13 +35,24 @@ export module bazel {
     }
 
 
+    /**
+     * Initialize the module with the user configuration.
+     * @param bazelConfig New configuration to load.
+     */
     function loadConfiguration(bazelConfig: WorkspaceConfiguration): void {
         bazelExecutablePath = bazelConfig.get<string>('executablePath') || 'bazel';
         excludedPackages = bazelConfig.get<string[]>('packageExcludes') || [];
     }
 
-    export async function queryBzl(cwd: string, query: string = '...'): Promise<BazelQueryItem[]> {
-        let excludedPackagesStr = excludedPackages.join(',');
+    /**
+     * Execute a Bazel query from a specific place.
+     * @param wd Working directory from where the bazel command must be launch.
+     * @param query Bazel query to execute.
+     * @returns List of all rules that have been found.
+     */
+    export async function queryBzl(wd: string, query: string = '...'): Promise<BazelQueryItem[]> {
+        const excludedPackagesStr = excludedPackages.join(',');
+        // Execute the bazel query
         let proc = exec(
             [
                 'query',
@@ -43,15 +62,17 @@ export module bazel {
                 `--deleted_packages=${excludedPackagesStr}`,
                 '--output', 'label_kind'
             ],
-            cwd
+            wd
         );
 
+        // Get the bazel query output
         let stdout:string = await proc.then(child => child.stdout);
         stdout = stdout.trim();
 
         let dependencies = stdout ? stdout.split('\n') : [];
         let separator = ' rule ';
 
+        // Search for all rule and return it
         let queries = dependencies.map((dependency: string): BazelQueryItem => {
             let idx = dependency.search(separator);
 
@@ -64,6 +85,12 @@ export module bazel {
         return queries;
     }
 
+    /**
+     * Generate for a specific target all the descriptors file.
+     * @param bzlWs Workspace property.
+     * @param target Target for with the descriptors files must be generate
+     * @returns List of all descriptor file path relative to the target.
+     */
     export function buildDescriptor(bzlWs: utils.BazelWorkspaceProperties, target:string): Promise<string[]> {
         return exec([
                 'build',
@@ -83,22 +110,47 @@ export module bazel {
         });
     }
 
-    export function build(terminal: Terminal, target: string) {
+    /**
+     * Execute a bazel build command in the given terminal.
+     * @param terminal Terminal to use for executing the build command.
+     * @param target Target that must be build.
+     */
+    export function build(terminal: Terminal, target: string): void {
         runInTerminal(terminal, [bazelExecutablePath, 'build', target]);
     }
 
-    export function run(terminal: Terminal, target: string) {
+    /**
+     * Execute a bazel run command in the given terminal.
+     * @param terminal Terminal to use for executing the run command.
+     * @param target Target that must be run.
+     */
+    export function run(terminal: Terminal, target: string): void {
         runInTerminal(terminal, [bazelExecutablePath, 'run', target]);
     }
 
+    /**
+     * Execute a bazel clean command in the given terminal.
+     * @param terminal Terminal to use for executing the clean command.
+     */
     export function clean(terminal: Terminal) {
         runInTerminal(terminal, [bazelExecutablePath, 'clean']);
     }
 
-    function runInTerminal(term: Terminal, cmd: string[]) {
-        term.sendText(cmd.join(' '), true);
+    /**
+     * Execute the given command in the terminal.
+     * @param terminal Terminal to use for executing the command.
+     * @param command Command to execute.
+     */
+    function runInTerminal(terminal: Terminal, command: string[]): void {
+        terminal.sendText(command.join(' '), true);
     }
 
+    /**
+     * Execute a bazel command with the following args from the specified directory.
+     * @param args Arguments for the bazel command.
+     * @param ws Working directory from where the bazel command must be execute.
+     * @returns The bazel stderr and stdout.
+     */
     function exec(args: string[], ws: string): Promise<{ stdout:string, stderr:string }> {
         return child_proc.exec(`"${bazelExecutablePath}" ${args.join(' ')}`, { cwd: ws });
     }
